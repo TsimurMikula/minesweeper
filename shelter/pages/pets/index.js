7a91b2417f443a8622634f6e170fe968ab760d93
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-04-03 01:51:10 +0300

    init: adding an adaptive menu to the shelter project

--- shelter/pages/pets/index.js
+++ shelter/pages/pets/index.js
@@ -1,1 +1,39 @@
 console.log('1. Вёрстка страницы Main соответствует макету при ширине экрана 1280px: +14\n2. Вёрстка страницы Main соответствует макету при ширине экрана 768px: +14\n3. Вёрстка страницы Main соответствует макету при ширине экрана 320px: +14\n4. Вёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6\n5. Вёрстка страницы Pets соответствует макету при ширине экрана 768px: +6\n6. Вёрстка страницы Pets соответствует макету при ширине экрана 320px: +6\n7. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20\n8. Верстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (Примеры неправильной и правильной реализации): +8\n9. При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4\n10. Верстка обеих страниц валидная: +8');
+
+//Burger menu start
+
+(function () {
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const blackOut = document.querySelectorAll('.c, .ourfriends');
+    const noScroll = document.querySelector('body');
+    const menuLinks = document.querySelectorAll('.header-item__link');
+    
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('header-nav-active');
+        burger.classList.toggle('burger-active');
+        blackOut.forEach(e => e.classList.toggle('blackOut-effect'));
+        noScroll.classList.toggle('body-no-scroll');
+    });
+    
+    for (let i = 0; i < menuLinks.length; i += 1) {
+        menuLinks[i].addEventListener('click', () => {
+            menu.classList.remove('header-nav-active');
+            burger.classList.toggle('burger-active');
+            blackOut.forEach(e => e.classList.remove('blackOut-effect'));
+            noScroll.classList.remove('body-no-scroll');
+        });    
+    }
+
+    document.addEventListener("click", (e) => {
+        if (menu.classList.contains('header-nav-active') && !burger.contains(e.target)  && !menu.contains(e.target)) {
+            menu.classList.remove('header-nav-active');
+            burger.classList.toggle('burger-active');
+            effectNO.forEach(e => e.classList.remove('notonly-effect'));
+            noScroll.classList.remove('body-no-scroll');
+        }
+    });
+
+}());
+
+//Burger menu end
