faa9b4b98fb362df15b4fc38b82af8bd34e41494
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-04-06 18:05:49 +0300

    feat:adding a slider to a shelter project update-2

--- shelter/pages/main/index.js
+++ shelter/pages/main/index.js
@@ -40,6 +40,8 @@ console.log('1. Вёрстка страницы Main соответствует 
 
 //Slider start
 
+import sliderCards from "./main.json" assert { type: "json" };
+
 (function () {
     const btn_left = document.querySelector('.arrow-left');
     const btn_right = document.querySelector('.arrow-right');
@@ -47,6 +49,31 @@ console.log('1. Вёрстка страницы Main соответствует 
     const cardsLeft = document.querySelector('#cards-left');
     const cardsRight = document.querySelector('#cards-right');
     
+    function init () {
+      const arr1 = [];
+      const arr2 = [];
+      const arr3 = [];
+
+        while (true) {        
+          let r = Math.floor(Math.random() * 8);      
+          if (arr1.length == 3) break;
+          else if (!arr1.includes(r)) arr1.push(r);
+        }
+        while (true) {        
+          let r = Math.floor(Math.random() * 8);      
+          if (arr2.length == 3) break;
+          else if (!arr2.includes(r) && !arr1.includes(r)) arr2.push(r);
+        }
+        while (true) {        
+          let r = Math.floor(Math.random() * 8);      
+          if (arr3.length == 3) break;
+          else if (!arr3.includes(r) && !arr2.includes(r)) arr3.push(r);
+        }
+
+        return [arr1, arr2, arr3];
+    }
+
+    console.log(init());
 
 
     const animLeft = () => {
@@ -64,103 +91,28 @@ console.log('1. Вёрстка страницы Main соответствует 
     btn_right.addEventListener('click', animRight);
 
     slider.addEventListener('animationend', (animationEvent) => {
-            const arrCards = [
-                {
-                  "name": "Jennifer",
-                  "img": "./assets/img/jennifer.png",
-                  "type": "Dog",
-                  "breed": "Labrador",
-                  "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
-                  "age": "2 months",
-                  "inoculations": ["none"],
-                  "diseases": ["none"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Sophia",
-                  "img": "./assets/img/sophia.png",
-                  "type": "Dog",
-                  "breed": "Shih tzu",
-                  "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
-                  "age": "1 month",
-                  "inoculations": ["parvovirus"],
-                  "diseases": ["none"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Woody",
-                  "img": "./assets/img/woody.png",
-                  "type": "Dog",
-                  "breed": "Golden Retriever",
-                  "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
-                  "age": "3 years 6 months",
-                  "inoculations": ["adenovirus", "distemper"],
-                  "diseases": ["right back leg mobility reduced"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Scarlett",
-                  "img": "./assets/img/scarlett.png",
-                  "type": "Dog",
-                  "breed": "Jack Russell Terrier",
-                  "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
-                  "age": "3 months",
-                  "inoculations": ["parainfluenza"],
-                  "diseases": ["none"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Katrine",
-                  "img": "./assets/img/katrine.png",
-                  "type": "Cat",
-                  "breed": "British Shorthair",
-                  "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
-                  "age": "6 months",
-                  "inoculations": ["panleukopenia"],
-                  "diseases": ["none"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Timmy",
-                  "img": "./assets/img/timmy.png",
-                  "type": "Cat",
-                  "breed": "British Shorthair",
-                  "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
-                  "age": "2 years 3 months",
-                  "inoculations": ["calicivirus", "viral rhinotracheitis"],
-                  "diseases": ["kidney stones"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Freddie",
-                  "img": "./assets/img/freddie.png",
-                  "type": "Cat",
-                  "breed": "British Shorthair",
-                  "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
-                  "age": "2 months",
-                  "inoculations": ["rabies"],
-                  "diseases": ["none"],
-                  "parasites": ["none"]
-                },
-                {
-                  "name": "Charly",
-                  "img": "./assets/img/charly.png",
-                  "type": "Dog",
-                  "breed": "Jack Russell Terrier",
-                  "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
-                  "age": "8 years",
-                  "inoculations": ["bordetella bronchiseptica", "leptospirosis"],
-                  "diseases": ["deafness", "blindness"],
-                  "parasites": ["lice", "fleas"]
-                }
-              ]
-
-
+          
         if (animationEvent.animationName === 'anim-left') {
             slider.classList.remove('slider-anim-left');
 
             const cardsLeftHtml = cardsLeft.innerHTML;
             document.querySelector('#cards-center').innerHTML = cardsLeftHtml;
+
+          function init () {
+            const arr1 = [];
+            const arr2 = [];
+            const arr3 = [];
+            let r = Math.floor(Math.random() * 8);
+
+              while (true) {              
+                if (arr3.length == 3) break;
+                else if (!arr3.includes(r)) arr3.push(r);
+              }
+              return arr3;
+          }
+
+          console.log(init());
+
 
             // Card1
             const card1 = document.createElement('div');
@@ -169,12 +121,12 @@ console.log('1. Вёрстка страницы Main соответствует 
             const random1 = Math.floor(Math.random() * 8);
 
             const card1Img = document.createElement('img');
-            card1Img.src = `${arrCards[random1]['img']}`;
+            card1Img.src = `${sliderCards[random1]['img']}`;
             card1.prepend(card1Img);
 
             const card1Name = document.createElement('h3');
             card1Name.classList.add('ourfriends-card__name');
-            card1Name.innerText = arrCards[random1]['name'];
+            card1Name.innerText = sliderCards[random1]['name'];
             card1.append(card1Name);
 
             const card1Btn = document.createElement('button');
@@ -189,12 +141,12 @@ console.log('1. Вёрстка страницы Main соответствует 
             const random2 = Math.floor(Math.random() * 8);
 
             const card2Img = document.createElement('img');
-            card2Img.src = `${arrCards[random2]['img']}`;
+            card2Img.src = `${sliderCards[random2]['img']}`;
             card2.prepend(card2Img);
 
             const card2Name = document.createElement('h3');
             card2Name.classList.add('ourfriends-card__name');
-            card2Name.innerText = arrCards[random2]['name'];
+            card2Name.innerText = sliderCards[random2]['name'];
             card2.append(card2Name);
 
             const card2Btn = document.createElement('button');
@@ -202,19 +154,19 @@ console.log('1. Вёрстка страницы Main соответствует 
             card2Btn.innerText = 'Learn more';
             card2.append(card2Btn);
 
-             // Card2
+             // Card3
              const card3 = document.createElement('div');
              card3.classList.add('ourfriends-card');
  
              const random3 = Math.floor(Math.random() * 8);
  
              const card3Img = document.createElement('img');
-             card3Img.src = `${arrCards[random3]['img']}`;
+             card3Img.src = `${sliderCards[random3]['img']}`;
              card3.prepend(card3Img);
  
              const card3Name = document.createElement('h3');
              card3Name.classList.add('ourfriends-card__name');
-             card3Name.innerText = arrCards[random3]['name'];
+             card3Name.innerText = sliderCards[random3]['name'];
              card3.append(card3Name);
  
              const card3Btn = document.createElement('button');
@@ -234,6 +186,72 @@ console.log('1. Вёрстка страницы Main соответствует 
             const cardsRightHtml = cardsRight.innerHTML;
 
             document.querySelector('#cards-center').innerHTML = cardsRightHtml;
+
+              // Card1
+              const card1 = document.createElement('div');
+              card1.classList.add('ourfriends-card');
+  
+              const random1 = Math.floor(Math.random() * 8);
+  
+              const card1Img = document.createElement('img');
+              card1Img.src = `${sliderCards[random1]['img']}`;
+              card1.prepend(card1Img);
+  
+              const card1Name = document.createElement('h3');
+              card1Name.classList.add('ourfriends-card__name');
+              card1Name.innerText = sliderCards[random1]['name'];
+              card1.append(card1Name);
+  
+              const card1Btn = document.createElement('button');
+              card1Btn.classList.add('ourfriends-card__button');
+              card1Btn.innerText = 'Learn more';
+              card1.append(card1Btn);
+  
+              // Card2
+              const card2 = document.createElement('div');
+              card2.classList.add('ourfriends-card');
+  
+              const random2 = Math.floor(Math.random() * 8);
+  
+              const card2Img = document.createElement('img');
+              card2Img.src = `${sliderCards[random2]['img']}`;
+              card2.prepend(card2Img);
+  
+              const card2Name = document.createElement('h3');
+              card2Name.classList.add('ourfriends-card__name');
+              card2Name.innerText = sliderCards[random2]['name'];
+              card2.append(card2Name);
+  
+              const card2Btn = document.createElement('button');
+              card2Btn.classList.add('ourfriends-card__button');
+              card2Btn.innerText = 'Learn more';
+              card2.append(card2Btn);
+  
+               // Card3
+               const card3 = document.createElement('div');
+               card3.classList.add('ourfriends-card');
+   
+               const random3 = Math.floor(Math.random() * 8);
+   
+               const card3Img = document.createElement('img');
+               card3Img.src = `${sliderCards[random3]['img']}`;
+               card3.prepend(card3Img);
+   
+               const card3Name = document.createElement('h3');
+               card3Name.classList.add('ourfriends-card__name');
+               card3Name.innerText = sliderCards[random3]['name'];
+               card3.append(card3Name);
+   
+               const card3Btn = document.createElement('button');
+               card3Btn.classList.add('ourfriends-card__button');
+               card3Btn.innerText = 'Learn more';
+               card3.append(card3Btn);
+  
+  
+              cardsRight.innerHTML = '';
+              cardsRight.appendChild(card1);
+              cardsRight.appendChild(card2);
+              cardsRight.appendChild(card3);
         }
 
         btn_left.addEventListener('click', animLeft);
